313b1b5be44c83ea67e5651b645252368cf5d3fc
Amrit0044 <premoberoi4444@gmail.com>
2025-08-31 22:07:33 +0530

    Create Experiment 1.2.c

---
+++ Experiment 01/Experiment 1.2.c
@@ -0,0 +1,12 @@
+*Experiment 1: Installation, Environment Setup and starting with C language
+2.Write a C Program to print the address in multiple lines (new line)*/
+#include <stdio.h>
+int main() {
+printf("Name - Amrit Raj\nSAP ID - 590027589\nCourse-BSC_CS\nBatch-01\n");
+printf("\n------------------------\n");
+printf("Amrit Raj\n");
+printf("Gr. Noida\n");
+printf("sector-omni-3\n");
+printf("Pincode-201310\n");
+return 0;
+}
