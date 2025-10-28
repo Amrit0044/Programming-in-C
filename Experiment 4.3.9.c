48f124101352ae363c1a4316c6675cc743659133
Amrit0044 <premoberoi4444@gmail.com>
2025-10-28 19:10:52 +0530

    Create Experiment 4.3.9.c

---
+++ Experiment 4.3.9.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+printf("Amrit Raj\nSAP ID: 590027589\nCourse: BSc_CS \nBatch: 1");
+printf("\n-----------------\n");
+printf("The pattern is:\n");
+int i, j;
+for(i=1; i<=5; i++)
+{
+int k=i;
+for(j=1; j<=i; j++)
+{
+printf("%d", k);
+k--;
+}
+printf("\n");
+}
+return 0;
+}
